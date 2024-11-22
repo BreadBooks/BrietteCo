989521f4ad8326f4d0d19d9d4b40bf0b4e2fd975
BreadBlocks <brett.boggs5104@gmail.com>
2024-11-22 15:37:38 -0600

    Minor AboutMe Fix

--- src/pages/TestimonialsPage.jsx
+++ src/pages/TestimonialsPage.jsx
@@ -13,7 +13,8 @@ function TestimonialsPage() {
             Hi there! I’m a Texas-born photographer with a love for capturing the little things that make life special. <br />
             I’ve been taking professional photos since 2023, and while I specialize in portraits—like family photos, graduations, engagements, and weddings—<br />
             I’m always excited to try something new and keep learning.
-            For me, photography is about preserving moments that matter, whether it’s a big celebration or a quiet, everyday memory. I love experimenting with creative ideas and seeing where they take me, so if you have a vision, let’s bring it to life!
+            For me, photography is about preserving moments that matter, whether it’s a big celebration or a quiet, everyday memory. <br />
+            I love experimenting with creative ideas and seeing where they take me, so if you have a vision, let’s bring it to life!
             </p>
             <p>
                 Thanks for stopping by—I can’t wait to help you capture memories that will make your heart smile!
