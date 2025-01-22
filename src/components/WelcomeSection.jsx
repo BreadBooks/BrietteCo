2c9451a2dc54fbf2a3d7e46d8fbde76b83591b10
BreadBlocks <brett.boggs5104@gmail.com>
2025-01-21 22:28:20 -0600

    Welcome section bio updated

--- src/components/WelcomeSection.jsx
+++ src/components/WelcomeSection.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import './WelcomeSection.css';
 
-
-
 function WelcomeSection() {
   return (
     <section className="welcome-section">
-      {/* No img element needed when using background in CSS */}
+      <div className="welcome-text">
+        <h1>Welcome to Briette & Co.!</h1>
+        <p>
+          I'm a passionate portrait photographer based in the heart of Dallas-Fort Worth.
+          <br />
+          My mission is to capture authentic moments and create timeless images that tell your unique story.
+          Whether it's a family portrait, a personal milestone, or just a spontaneous moment, I love turning memories into art that will last a lifetime.
+        </p>
+        <p>
+          Thank you for considering me to preserve your special moments—I can't wait to meet you and craft beautiful memories together!
+        </p>
+        <p className="contact-link">
+          If you'd like to get in contact,{' '}
+          <a href="/contact" className="contact-link-text">
+            click here!
+          </a>
+        </p>
+      </div>
     </section>
   );
 }
-
 
 export default WelcomeSection;
