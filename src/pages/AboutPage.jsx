57f509549b0bed36743eb4b141450eb7a182e7dc
BreadBlocks <brett.boggs5104@gmail.com>
2025-02-13 13:35:35 -0600

    redirect added to about me

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,6 @@
+// src/pages/AboutPage.jsx
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link if using React Router
 import './AboutMe.css'; 
 import image1 from '../assets/about1.svg';
 import image2 from '../assets/about2.svg';
@@ -9,7 +11,12 @@ function AboutPage() {
     <div className="about-me-images">
       <img src={image1} alt="About me image 1" className="page-wide-image" />
       <img src={image2} alt="About me image 2" className="page-wide-image" />
-      <img src={image3} alt="About me image 3" className="page-wide-image" />
+      <div className="contact-overlay-container">
+        <img src={image3} alt="About me image 3" className="page-wide-image" />
+        <Link to="/contact" className="overlay-text">
+          Book Your Session Today!
+        </Link>
+      </div>
     </div>
   );
 }
