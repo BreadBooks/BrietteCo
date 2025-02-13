f447b6ae1f3d41592b1cf8554e6798f80e07c408
BreadBlocks <brett.boggs5104@gmail.com>
2025-02-13 15:31:50 -0600

    cf fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()]
+    plugins: [react()],
+    base: '/', // Change this if needed (see explanation below)
+    build: {
+        outDir: 'dist', // Ensure the build goes into `dist/`
+    },
 });
